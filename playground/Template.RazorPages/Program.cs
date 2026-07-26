6fca8aca67a83d602883a985d84c0064facd437c
speegs92 <christian@levesque.dev>
2026-07-26 13:14:50 -0500

    added razor pages project

---
+++ playground/Template.RazorPages/Program.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+
+var app = builder.Build();
+
+app.UseHttpsRedirection();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapStaticAssets();
+app
+	.MapRazorPages()
+	.WithStaticAssets();
+
+app.Run();
