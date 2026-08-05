50bb909dcf493b9afa265bc865fa3b56847d6f7d
speegs92 <christian@levesque.dev>
2026-08-05 18:04:06 -0500

    added additional configurers

---
+++ src/Sienar.Plugins.Mvc/Configuration/DefaultAuthenticationBuilderConfigurer.cs
@@ -0,0 +1,22 @@
+﻿#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
+
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+
+namespace Sienar.Infrastructure;
+
+/// <exclude />
+public class DefaultAuthenticationBuilderConfigurer
+	: IConfigurer<AuthenticationBuilder>
+{
+	public void Configure(AuthenticationBuilder options)
+	{
+		options.AddCookie(
+			CookieAuthenticationDefaults.AuthenticationScheme,
+			o =>
+			{
+				o.LoginPath = DashboardUrls.Account.Login;
+				o.AccessDeniedPath = DashboardUrls.Account.Forbidden;
+			});
+	}
+}
---
+++ src/Sienar.Plugins.Mvc/Configuration/DefaultAuthenticationConfigurer.cs
@@ -0,0 +1,17 @@
+﻿#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
+
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+
+namespace Sienar.Infrastructure;
+
+/// <exclude />
+public class DefaultAuthenticationConfigurer : IConfigurer<AuthenticationOptions>
+{
+	public void Configure(AuthenticationOptions options)
+	{
+		options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+		options.DefaultChallengeScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+		options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+	}
+}
