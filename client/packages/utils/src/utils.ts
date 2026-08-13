ad5aa2d2c5305fc9bd05b81a52f0114d490c847c
speegs92 <christian@levesque.dev>
2026-08-13 15:20:47 -0500

    removed CSRF token refreshing from the client since this is handled on the server

--- client/packages/utils/src/utils.ts
+++ client/packages/utils/src/utils.ts
@@ -20,10 +20,7 @@ export async function sendRequest<T>(
 	const init: RequestInit = Object.assign({ method, body }, requestOptions);
 
 	const safeMethods: HttpMethod[] = ['GET', 'HEAD', 'OPTIONS', 'CONNECT'];
-	let shouldRefreshToken = false;
-
 	if (!safeMethods.includes(method)) {
-		shouldRefreshToken = true;
 		init.headers = Object.assign({
 			'X-XSRF-TOKEN': getCookie('XSRF-TOKEN')
 		}, init.headers);
@@ -83,12 +80,6 @@ export async function sendRequest<T>(
 
 	requestResult.wasSuccessful = response.ok;
 	requestResult.result = result.result;
-
-	if (shouldRefreshToken) {
-		try {
-			await fetch('/api/csrf');
-		} catch {}
-	}
 
 	return requestResult;
 }
