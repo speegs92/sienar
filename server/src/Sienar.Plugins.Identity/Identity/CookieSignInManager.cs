393092b272bb74f634001ef4c06ae1eb5fe63671
speegs92 <christian@levesque.dev>
2026-08-13 15:15:29 -0500

    updated CookieSignInManager to update the CSRF token on login and logout

--- server/src/Sienar.Plugins.Identity/Identity/CookieSignInManager.cs
+++ server/src/Sienar.Plugins.Identity/Identity/CookieSignInManager.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Authentication;
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
@@ -9,15 +10,18 @@ public class CookieSignInManager<T> : ISignInManager<T>
 	where T : class, ISienarIdentityUser<T>
 {
 	private readonly IHttpContextAccessor _contextAccessor;
+	private readonly ICsrfTokenRefresher _csrfTokenRefresher;
 	private readonly LoginOptions _loginOptions;
 	private readonly IUserClaimsPrincipalFactory<T> _principalFactory;
 
 	public CookieSignInManager(
 		IHttpContextAccessor contextAccessor,
+		ICsrfTokenRefresher csrfTokenRefresher,
 		IOptions<LoginOptions> loginOptions,
 		IUserClaimsPrincipalFactory<T> principalFactory)
 	{
 		_contextAccessor = contextAccessor;
+		_csrfTokenRefresher = csrfTokenRefresher;
 		_loginOptions = loginOptions.Value;
 		_principalFactory = principalFactory;
 	}
@@ -32,17 +36,23 @@ public class CookieSignInManager<T> : ISignInManager<T>
 			IssuedUtc = DateTimeOffset.UtcNow,
 			ExpiresUtc = GetExpiration(isPersistent)
 		};
+
 		var claimsPrincipal = await _principalFactory.CreateAsync(user);
 		await _contextAccessor.HttpContext!.SignInAsync(
 			CookieAuthenticationDefaults.AuthenticationScheme,
 			claimsPrincipal,
 			authProperties);
+
+		_contextAccessor.HttpContext!.User = claimsPrincipal;
+		_csrfTokenRefresher.RefreshToken();
 	}
 
 	public async Task SignOut()
 	{
 		await _contextAccessor.HttpContext!.SignOutAsync(
 			CookieAuthenticationDefaults.AuthenticationScheme);
+		_contextAccessor.HttpContext!.User = new ClaimsPrincipal();
+		_csrfTokenRefresher.RefreshToken();
 	}
 
 	private DateTimeOffset GetExpiration(bool isPersistent)
