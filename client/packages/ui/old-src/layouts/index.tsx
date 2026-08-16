1ab164275302253e81b7165af738ab195705c7d7
speegs92 <christian@levesque.dev>
2026-08-16 11:53:04 -0500

    removed old MUI-based components from @sienar/ui

--- client/packages/ui/old-src/layouts/index.tsx
+++
@@ -1,3 +0,0 @@
-﻿export { default as Dashboard } from './Dashboard.tsx';
-export { default as DashboardNarrow } from './DashboardNarrow.tsx';
-export { default as DashboardShared } from './DashboardShared.tsx';
--- client/packages/ui/old-src/pages/index.ts
+++
@@ -1,4 +0,0 @@
-﻿export * from './LoadingPage.tsx';
-export { default as LoadingPage } from './LoadingPage.tsx';
-export * from './StatusPage.tsx';
-export { default as StatusPage } from './StatusPage.tsx';
