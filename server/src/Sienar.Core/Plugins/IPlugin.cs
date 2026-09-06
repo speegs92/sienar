f6dcfa1ca9b755bb51f07ab290333634db6f9045
speegs92 <christian@levesque.dev>
2026-09-06 12:26:54 -0500

    changed IPlugin to be an IConfigurer<SienarApplicationBuilder>

--- server/src/Sienar.Core/Plugins/IPlugin.cs
+++ server/src/Sienar.Core/Plugins/IPlugin.cs
@@ -3,29 +3,4 @@
 /// <summary>
 /// Represents a distributable plugin for Sienar applications
 /// </summary>
-public interface IPlugin
-{
-	/// <summary>
-	/// Configures the Sienar application
-	/// </summary>
-	/// <param name="builder">The Sienar application builder</param>
-	void ConfigureSienar(SienarApplicationBuilder builder);
-
-	/// <summary>
-	/// Configures the underlying application builder
-	/// </summary>
-	/// <param name="adapter">The host application builder</param>
-	/// <param name="sp">The startup service provider</param>
-	void ConfigureBuilder(
-		IBuilderAdapter adapter,
-		IServiceProvider sp);
-
-	/// <summary>
-	/// Configures the application
-	/// </summary>
-	/// <param name="adapter">The host application adapter</param>
-	/// <param name="sp">The startup service provider</param>
-	void ConfigureApplication(
-		HostAdapter adapter,
-		IServiceProvider sp);
-}
+public interface IPlugin : IConfigurer<SienarApplicationBuilder>;
