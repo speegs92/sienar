adff3bc5d6c498f66016d9bd953a7a1cd2724887
speegs92 <christian@levesque.dev>
2026-09-06 13:11:59 -0500

    refactored static assets plugin to use configurers

--- server/src/Sienar.Plugins.Mvc/Plugins/SienarStaticAssetsPlugin.cs
+++ server/src/Sienar.Plugins.Mvc/Plugins/SienarStaticAssetsPlugin.cs
@@ -1,4 +1,6 @@
-﻿namespace Sienar.Plugins;
+﻿using Sienar.Configuration.StaticAssets;
+
+namespace Sienar.Plugins;
 
 /// <summary>
 /// Configures the Sienar application to use ASP.NET static asset mapping middleware
@@ -6,27 +8,8 @@
 public class SienarStaticAssetsPlugin : IPlugin
 {
 	/// <inheritdoc />
-	public void ConfigureSienar(SienarApplicationBuilder builder) {}
-
-	/// <inheritdoc />
-	public void ConfigureBuilder (
-		IBuilderAdapter adapter,
-		IServiceProvider sp) {}
-
-	/// <inheritdoc />
-	public void ConfigureApplication(
-		HostAdapter adapter,
-		IServiceProvider sp)
+	public void Configure(SienarApplicationBuilder builder)
 	{
-		if (adapter.Host is not WebApplication webapp)
-		{
-			throw new InvalidOperationException($"The {nameof(SienarStaticAssetsPlugin)} only works with ASP.NET web applications.");
-		}
-
-		var middlewareProvider = adapter.Services.GetRequiredService<MiddlewareProvider>();
-
-		middlewareProvider.AddWithPriority(
-			MvcMiddlewarePriorities.WithStaticAssets,
-			() => webapp.MapStaticAssets());
+		builder.StartupServices.AddHostConfigurer<HostConfigurer>();
 	}
 }
