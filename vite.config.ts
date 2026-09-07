0f623fee1ddd51b00eb1bc0bcc1de8f9df502872
speegs92 <christian@levesque.dev>
2026-09-07 11:15:02 -0500

    moved react code to new Sienar.React project

--- vite.config.ts
+++ vite.config.ts
@@ -7,12 +7,12 @@ export default defineConfig({
 	plugins: [react()],
 	resolve: {
 		alias: {
-			'@sienar/utils': resolve(__dirname, './src/Sienar.Plugins.Mvc/wwwsrc/utils/src/index.ts'),
-			'@utils': resolve(__dirname, './src/Sienar.Plugins.Mvc/wwwsrc/utils/src'),
-			'@sienar/ui': resolve(__dirname, './src/Sienar.Plugins.Mvc/wwwsrc/ui/src/index.ts'),
-			'@ui': resolve(__dirname, './src/Sienar.Plugins.Mvc/wwwsrc/ui/src'),
-			'@sienar/plugins-core': resolve(__dirname, './src/Sienar.Plugins.Mvc/wwwsrc/plugins-core/src/index.ts'),
-			'@plugins-core': resolve(__dirname, './src/Sienar.Plugins.Mvc/wwwsrc/plugins-core/src'),
+			'@sienar/utils': resolve(__dirname, './src/Sienar.React/wwwsrc/utils/src/index.ts'),
+			'@utils': resolve(__dirname, './src/Sienar.React/wwwsrc/utils/src'),
+			'@sienar/ui': resolve(__dirname, './src/Sienar.React/wwwsrc/ui/src/index.ts'),
+			'@ui': resolve(__dirname, './src/Sienar.React/wwwsrc/ui/src'),
+			'@sienar/plugins-core': resolve(__dirname, './src/Sienar.React/wwwsrc/plugins-core/src/index.ts'),
+			'@plugins-core': resolve(__dirname, './src/Sienar.React/wwwsrc/plugins-core/src'),
 			'@sienar/plugins-identity': resolve(__dirname, './src/Sienar.Plugins.Identity/wwwsrc/src/index.ts'),
 			'@plugins-identity': resolve(__dirname, './src/Sienar.Plugins.Identity/wwwsrc/src')
 		}
