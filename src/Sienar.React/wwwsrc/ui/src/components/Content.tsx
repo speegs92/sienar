78412332ec2d6503c09078cb3e19832388abb38f
speegs92 <christian@levesque.dev>
2026-09-13 13:18:25 -0500

    added bulma content

---
+++ src/Sienar.React/wwwsrc/ui/src/components/Content.tsx
@@ -0,0 +1,42 @@
+﻿import { DynamicComponent } from '@ui/components/DynamicComponent.tsx';
+
+import type { ElementType, ReactElement } from 'react';
+import type { DynamicComponentProps } from '@ui/components/DynamicComponent.tsx';
+import type { Size } from '@ui/theme.ts';
+
+/**
+ * The props for the content component
+ */
+export type ContentProps<T extends ElementType = 'div'> = {
+	/**
+	 * The font size of the content
+	 */
+	size?: Size;
+} & DynamicComponentProps<T>;
+
+export function Content<T extends ElementType>(
+	props: ContentProps<T> & { tag: T }
+): ReactElement;
+
+export function Content(
+	props: ContentProps & { tag?: undefined }
+): ReactElement;
+
+export function Content<T extends ElementType = 'div'>(props: ContentProps<T>) {
+	const {
+		size,
+		...rest
+	} = props;
+
+	return (
+		<DynamicComponent
+			additionalClasses={[
+				'content',
+				{
+					[`is-${size}`]: !!size
+				}
+			]}
+			{...rest}
+		/>
+	);
+}
--- src/Sienar.React/wwwsrc/ui/src/components/index.ts
+++ src/Sienar.React/wwwsrc/ui/src/components/index.ts
@@ -3,6 +3,7 @@ export * from './Box.tsx';
 export * from './Button/index.ts';
 export * from './Column/index.ts';
 export * from './Container.tsx';
+export * from './Content.tsx';
 export * from './DynamicComponent.tsx';
 export * from './Footer.tsx';
 export * from './Hero/index.ts';
