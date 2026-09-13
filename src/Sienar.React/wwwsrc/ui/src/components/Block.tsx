f4113a15c829a4ac577920c59993b12b638745ef
speegs92 <christian@levesque.dev>
2026-09-13 13:07:58 -0500

    added bulma block

---
+++ src/Sienar.React/wwwsrc/ui/src/components/Block.tsx
@@ -0,0 +1,26 @@
+﻿import { DynamicComponent } from '@ui/components/DynamicComponent.tsx';
+
+import type { ElementType, ReactElement } from 'react';
+import type { DynamicComponentProps } from '@ui/components/DynamicComponent.tsx';
+
+/**
+ * The props for the block component
+ */
+export type HeroHeadProps<T extends ElementType = 'div'> = {} & DynamicComponentProps<T>;
+
+export function Block<T extends ElementType>(
+	props: HeroHeadProps<T> & { tag: T }
+): ReactElement;
+
+export function Block(
+	props: HeroHeadProps & { tag?: undefined }
+): ReactElement;
+
+export function Block<T extends ElementType = 'div'>(props: HeroHeadProps<T>) {
+	return (
+		<DynamicComponent
+			additionalClasses={['block']}
+			{...props}
+		/>
+	);
+}
--- src/Sienar.React/wwwsrc/ui/src/components/index.ts
+++ src/Sienar.React/wwwsrc/ui/src/components/index.ts
@@ -1,4 +1,5 @@
-﻿export * from './Button/index.ts';
+﻿export * from './Block.tsx';
+export * from './Button/index.ts';
 export * from './Column/index.ts';
 export * from './Container.tsx';
 export * from './DynamicComponent.tsx';
